7542deb793630c9db86bfe3ecfff09a1f7d8cd15
innayan <admin@mail.ru>
2016-05-11 19:51:34 +0300

    all

---
+++ src/main/java/pk1/AnotherClass.java
@@ -0,0 +1,46 @@
+package pk1;
+//tester1
+//dfgfgdgdfgd
+//dfdfdf
+//tytytyty
+//kjklkjlkjk
+//rgyrtytrytryry\//sdfsfsdfsdfsdf\//tytytyty
+//rtrtrttt
+//erreotieoritoeitpeoitpeoitpepoipoipoirpoirp
+//retetetetert
+public class AnotherClass {
+	int myResult;
+	int newResult;
+	int myResult1;
+	ClassForMath aaa;
+	//dsdfsf
+	
+	public int  AnotherClass(){
+ ClassForMath math1 = new ClassForMath();
+  myResult1=math1.multiply(5, 6);
+  return myResult1;
+  
+  //rtrtrtrt
+  //dfgfgdfgdfg
+	}
+	
+	public int  newMultiplicity(int x, int y){
+		  myResult=aaa.multiply(x, y);
+		  return myResult;
+	}
+		  
+	public int  NewSumma(int num)	{
+		newResult=myResult+num;
+		return newResult;
+	}
+	public void publish()
+	{
+		System.out.println (this.AnotherClass());
+		System.out.println (this.NewSumma(29));
+		System.out.println (this.newMultiplicity(2,9));
+	}
+		
+		
+
+
+}
